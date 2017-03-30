0d06f6049a104fb5e178f74e807e8353e82ceb09
qimei <qimei@192.168.0.10>
2017-03-30 13:20:12 +0800

    create userinputs to assemble all collected data from user to be used when updating details of any of the table

---
+++ src/org/qimei/usersbot/pojo/UserInputs.java
@@ -0,0 +1,33 @@
+package org.qimei.usersbot.pojo;
+
+public class UserInputs {
+	
+	User user;
+	String columnName;
+	Object columnValue;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getColumnName() {
+		return columnName;
+	}
+
+	public void setColumnName(String columnName) {
+		this.columnName = columnName;
+	}
+
+	public Object getColumnValue() {
+		return columnValue;
+	}
+
+	public void setColumnValue(Object columnValue) {
+		this.columnValue = columnValue;
+	}
+
+}
