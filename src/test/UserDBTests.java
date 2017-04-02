78791545975a304746a1c221fa733e0f3088497c
qimei <qimei@192.168.0.10>
2017-04-02 19:36:50 +0800

    done with jUnit test and DB created

--- src/test/UserDBTests.java
+++ src/test/UserDBTests.java
@@ -3,6 +3,7 @@ package test;
 import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.List;
+import java.util.UUID;
 
 import org.apache.log4j.Logger;
 import org.junit.After;
@@ -11,6 +12,9 @@ import org.junit.Test;
 import org.qimei.usersbot.db.UsersDBHelper;
 import org.qimei.usersbot.db.UsersDBManager;
 import org.qimei.usersbot.pojo.User;
+import org.qimei.usersbot.pojo.UserLoginHistory;
+import org.qimei.usersbot.pojo.UserNewsPreferences;
+import org.qimei.usersbot.pojo.UserPreferences;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -42,11 +46,54 @@ public class UserDBTests {
 		user.setId(null);
 		user.setEmailId("qimei24@gmail.com");
 		user.setFbId("1234");
-		user.setLastLoginTime(Calendar.getInstance().getTime());
+		user.setCreationDate(Calendar.getInstance().getTime());
 		user.setUserName("Tan Qi Mei");
 
 		int rowCount = dbHelper.insertIntoUsers(user);
 
+		assert (rowCount > 0);
+	}
+	
+	@Test
+	public void testInsertUserLoginHistory() throws SQLException {
+		
+		User user = new User();
+		UserLoginHistory userLoginHistory = new UserLoginHistory();
+		user.setId(UUID.fromString("db93d7cc-c171-4a82-b6d0-357611727172"));
+		userLoginHistory.setUser(user);
+		userLoginHistory.setLastLoginTime(Calendar.getInstance().getTime());
+		
+		int rowCount = dbHelper.insertIntoUserLoginHistory(userLoginHistory);
+		
+		assert (rowCount > 0);
+	}
+	
+	@Test
+	public void testInsertUserNewsPreferences() throws SQLException {
+		
+		User user = new User();
+		UserNewsPreferences userNewsPreferences = new UserNewsPreferences();
+		user.setId(UUID.fromString("db93d7cc-c171-4a82-b6d0-357611727172"));
+		userNewsPreferences.setUser(user);
+		userNewsPreferences.setArticleId("1231231");
+		userNewsPreferences.setViewTime(Calendar.getInstance().getTime());
+		
+		int rowCount = dbHelper.insertIntoUserNewsPreferences(userNewsPreferences);
+		
+		assert (rowCount > 0);
+	}
+	
+	@Test
+	public void testInsertUserPreferences() throws SQLException {
+		
+		User user = new User();
+		UserPreferences userPreferences = new UserPreferences();
+		user.setId(UUID.fromString("db93d7cc-c171-4a82-b6d0-357611727172"));
+		userPreferences.setUser(user);
+		userPreferences.setUserPreferences("Politics");
+		
+		int rowCount = dbHelper.insertIntoUserPreferences(userPreferences);
+		
 		assert (rowCount > 0);
 	}
 	
